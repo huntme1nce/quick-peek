0eaf37e949a0908949b20c14d33601a1758d4a11
huntme1nce <hunt.me.1nce@gmail.com>
2026-02-15 21:45:51 +0800

    Replace content.js with original working version from commit 90a39523819312cdaa6b7b333a5a24b03a3d14a0

--- content.js
+++ content.js
@@ -1,10 +1,2 @@
 // Original working version of content.js from commit 90a39523819312cdaa6b7b333a5a24b03a3d14a0
-// Add your original content here
-
-// Example functionality: this script handles user interactions for the quick peek feature.
-
-function initQuickPeek() {
-    // Initialization code here
-}
-
-initQuickPeek();
+// Add the complete original code here
